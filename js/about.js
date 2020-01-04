de3b8a949ce5ad38504f2e0d94e0fc83df33d109
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-01-04 16:43:31 +0100

    Math

--- js/about.js
+++ js/about.js
@@ -1,6 +1,7 @@
-window.onload = function () {
+ function total() {
     var table = document.getElementById("trips");
 
+    //sum the cols
     var i = 1;
     var sum = 0;
     while (i !== 5) {
@@ -12,4 +13,36 @@ window.onload = function () {
 
         i++;
     }
+
 }
+
+function maxMonth(){
+    //sum rows per month
+    var table = document.getElementById("trips");
+
+    var k=0;
+    var add=0;
+    var months = new this.Array(11);
+    console.log(months[0]);
+    if(months[0]==undefined)//check for not defined values of the array
+        months[0]=0;
+    while (k !== 10) {
+        for (var j = 1; j < 5; j++) 
+            add +=this.parseInt(table.rows[k + 1].cells[j].innerHTML);
+        
+        months[k+1]=add;
+        add = 0;
+
+        k++;
+    }
+    var maxMonth=Math.max.apply(null,months);
+    var index=months.indexOf(maxMonth);
+    // console.log(index);
+    document.getElementById("max").innerHTML=table.rows[index].cells[0].innerHTML;
+}
+
+window.onload=function(){
+    total();
+    maxMonth();
+}
+
