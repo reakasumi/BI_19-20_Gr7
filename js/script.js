a3bcf066e4d6c55582672ddc65d4bb41e1d29e0b
QendresaB <38337116+QendresaB@users.noreply.github.com>
2020-01-05 04:19:25 +0100

    changes on slider

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 
-var slideIndex = 1;
-showSlides(slideIndex);
+//var slideIndex = 1;
+// showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-	showSlides((slideIndex += n));
-}
-
+// function plusSlides(n) {
+	// showSlides((slideIndex += n));
+// }
+var stop = true;
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
+  stop=false;
 }
 
 function showSlides(n) {
@@ -26,12 +27,31 @@ function showSlides(n) {
 		slides[i].style.display = "none";
 	}
 	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
+		dots[i].className = dots[i].className.replace("active", "");
   }
   try{
 	  slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";} 
     catch(e){
       console.log("Error, first time rendered the value is undefined!")
-    }
+	}
+	
+	  
 }
+
+        var slideIndex = 0;
+         slideShow();
+        function slideShow() {
+			if(this.stop)
+       { var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+		setTimeout(slideShow, 3000); }// Change image every 2 seconds
+		else{
+		}
+        }
