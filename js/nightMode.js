1c5984030587f90411139ffbb4bcb97133a51fa7
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-01-05 19:00:34 +0100

    updt

--- js/nightMode.js
+++ js/nightMode.js
@@ -3,14 +3,23 @@ var time = date.getHours();
 //console.log(time);
 // var night=document.getElementById("modes");
 
+var clicks=0;
+
 function changeMode() {
+    clicks++;
+
     //time has values 0-23
-    if (time >= 20 || time<=6) {
+    if (clicks>1) {
+        window.alert("You already changed the color");
+        
+    }
+    else if(time>6 || time<20){
+        document.querySelector("#modes").innerHTML="Day Mode On";
+        document.body.style.backgroundColor="#F0F8FF";
+    }
+    else if(time >= 20 || time<=6){
         document.querySelector("#modes").innerHTML="Night Mode On";
         document.body.style.backgroundColor = "#F5F5DC";
     }
-    else{
-        document.querySelector("#modes").innerHTML="Day Mode On";
-        document.body.style.backgroundColor="white";
-    }
+    
 }
