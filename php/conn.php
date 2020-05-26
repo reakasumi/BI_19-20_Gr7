2006dd13c5d30f06bf389d79d6646ae00d995141
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-05-26 22:23:08 +0200

    changes

--- php/conn.php
+++ php/conn.php
@@ -11,18 +11,26 @@
         die('Nuk mund te lidhet databaza: ' . mysqli_connect_error());
     }
     echo 'Lidhja e suksesshme<br>';
-    
+
     $sql = 'CREATE TABLE user (
         Id integer, 
         UserName varchar(20),
-        Pass varchar(20)';
+        Pass varchar(20))';
 
 
-    $retval = mysqli_query( $conn, $sql );
+   // $retval = mysqli_query( $conn, $sql );
+    // if(! $retval )
+    // {
+    // die('Nuk mund te krijohet tabela' . mysqli_connect_error());
+    // }
+    // echo "Tabela u krijua me sukses!\n";
+
+    $sql1 = 'insert into user values(1,"filan","iamugly");';
+    $retval = mysqli_query( $conn, $sql1 );
     if(! $retval )
     {
-    die('Nuk mund te krijohet tabela' . mysqli_connect_error());
+    die('Te dhenat nuk mund te shtohen' . mysqli_connect_error());
     }
-    echo "Tabela u krijua me sukses!\n";
+    echo "Te dhenat u shtuan!\n";
     mysqli_close($conn);
 ?>
