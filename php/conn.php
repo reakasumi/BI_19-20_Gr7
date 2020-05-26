8aef27882ded8b75e6d357cf58ae2253c6d0dfc4
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-05-26 21:38:38 +0200

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -2,9 +2,11 @@
 	$dbhost = 'localhost:3316';
 	$dbuser = 'root';
 	$dbpass = '';
-	$db='travelDB';
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+	//$db='travelDB';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
     
+    $sql = 'CREATE Database travelDB';
+$retval = mysqli_query( $conn, $sql );
     if(! $conn ){
         die('Nuk mund te lidhet databaza: ' . mysqli_connect_error());
     }
