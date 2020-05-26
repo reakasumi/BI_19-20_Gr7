9717eebde000a5a191607895e9dc797a16acda86
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-05-26 22:10:51 +0200

    login users table

--- php/conn.php
+++ php/conn.php
@@ -2,14 +2,27 @@
 	$dbhost = 'localhost:3316';
 	$dbuser = 'root';
 	$dbpass = '';
-	//$db='travelDB';
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+	$db='travelDB';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
     
-    $sql = 'CREATE Database travelDB';
-$retval = mysqli_query( $conn, $sql );
+    //$sql = 'CREATE Database travelDB';
+    //$retval = mysqli_query( $conn, $sql );
     if(! $conn ){
         die('Nuk mund te lidhet databaza: ' . mysqli_connect_error());
     }
-    echo 'Lidhja e suksesshme';
+    echo 'Lidhja e suksesshme<br>';
+    
+    $sql = 'CREATE TABLE user (
+        Id integer, 
+        UserName varchar(20),
+        Pass varchar(20)';
+
+
+    $retval = mysqli_query( $conn, $sql );
+    if(! $retval )
+    {
+    die('Nuk mund te krijohet tabela' . mysqli_connect_error());
+    }
+    echo "Tabela u krijua me sukses!\n";
     mysqli_close($conn);
 ?>
